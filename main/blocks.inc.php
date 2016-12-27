3b94015b1de135a905b69e4bcaff5bc813352376
Maik <mail@web-rider.de>
2016-12-27 20:33:27 +0800

    Init Main module

---
+++ main/blocks.inc.php
@@ -0,0 +1,399 @@
+<?php
+
+$_lk_faq_node = NULL;
+$_lk_subtitle = NULL;
+$_lk_icon = NULL;
+
+
+
+function lokalkoenig_admin_block_info(){
+
+ $blocks['lokalkoenig_title'] = array(
+    // info: The name of the block.
+    'info' => 'Block: Title der Seite',
+    // Block caching options (per role, per user, etc.)
+  );
+
+  $blocks['lokalkoenig_admin_block'] = array(
+    // info: The name of the block.
+    'info' => 'Lokalkoenig Moderation links',
+    // Block caching options (per role, per user, etc.)
+  );
+ 
+return $blocks;
+}
+
+
+
+
+function lokalkoenig_admin_block_view($delta = '') {
+global $user, $_lk_subtitle, $_lk_icon, $_lk_faq_node;
+  
+  $block = array();
+ 
+  if($delta === 'lokalkoenig_admin_block'):
+     // new Block for Mods
+      if(arg(0) == "lkadmin" OR arg(0) == "logbuch" OR arg(0) == "verlage" OR arg(0) == "lkstats" OR arg(0) == "kampagnen" OR arg(0) == "lkabrechnung"):
+           return array('title' => "", 'content' => theme('lokalkoenig_admin_navigation'));
+       endif;
+    endif;
+  
+ 
+  
+  if($delta == 'lokalkoenig_title'){
+    $content = '<div class="page-header">';
+    
+    if((arg(0) == 'merkliste3' AND arg(1)) OR (arg(0) == "node" AND $node = node_load(arg(1)) AND $node -> type == 'kampagne' AND !arg(2))){
+      $content .=  '<div class="pull-right" style="margin-top: 0px; font-size: 2em;"><a href="javascript:history.back()" style="font-size: 0.7em; color: White;">Zurück <span class="glyphicon glyphicon-chevron-right"></span></a></div>';
+    }
+    $content .= '<h2>';
+    
+   $title = drupal_get_title();
+    
+    if($user -> uid != 0){
+      if(arg(0) == "user" AND !arg(2) AND $account = user_load(arg(1)) AND $account -> uid != 0){
+        //$title = lk_get_username_and_role($account); 
+      }
+    }
+    
+    if(arg(0) == "node" AND $node = node_load(arg(1)) AND !arg(2)){
+    
+      
+      if($node -> type == 'kampagne' AND $node -> status == 1){
+        $title = 'Detailansicht'; 
+        $content .= '<span class="glyphicon glyphicon-plus"></span> ';
+      }
+    }
+    
+    if($_lk_icon){
+      $content .= '<span class="glyphicon glyphicon-'. $_lk_icon .'"></span> ';
+    }  
+      
+    $content .= $title;
+    
+    if($_lk_subtitle){
+      $content .= ' <small>' . $_lk_subtitle . '</small>';
+    }
+    
+    $content .= '</h2></div>';
+    
+    return array('content' => $content);
+  }
+}  
+
+
+function lk_get_subtitle(){
+    return $GLOBALS["_lk_subtitle"];
+}
+
+function lk_set_subtitle($subtitle = NULL){
+global $_lk_subtitle;
+  $_lk_subtitle = $subtitle;
+}
+
+function lk_set_icon($icon = NULL){
+global $_lk_icon;
+
+  $_lk_icon = $icon;
+}
+
+function lk_set($pagetitle, $subtitle, $icon, $faq_id = NULL){
+   lk_set_faq($faq_id);
+   drupal_set_title($pagetitle);
+   lk_set_subtitle($subtitle);
+   lk_set_icon($icon);
+}
+
+function lokalkoenig_init(){
+global $user; 
+   if(arg(0) == 'user'){
+      lk_set_faq(6);
+      
+      
+      if($user -> uid == 0){
+        drupal_set_title("Anmelden");
+         lk_set_icon('home');
+      }
+      else {
+        lk_set_icon('user');
+        drupal_set_title("Ihr Profil");
+        //lk_set_subtitle('Verwalten Sie hier Ihre Daten');
+      }
+      
+   }
+}
+
+function pathtitle($path, $special = NULL){
+  
+  $node = new stdClass();
+  $node -> title = 'Lokalkönig';
+  $node -> type = 'dummy';
+  
+  if(arg(0) == "node"){
+    $nodel = node_load(arg(1));
+    if($nodel){
+       //dpm($nodel);
+       
+       $node = $nodel;
+    // Kampagnen-Typ
+    if($node -> type == "kampagne" AND !$special){
+        
+        if(isset($node->field_kamp_preisnivau['und'][0]['tid'])){
+            $tax = taxonomy_term_load($node->field_kamp_preisnivau['und'][0]['tid']);
+            $special = '<span class="label label-success">'. $tax -> name .'</span>';
+        }
+        
+        if(lk_is_moderator()){
+          $special .= ' <span class="label label-info">'. strtoupper($node -> lkstatus) .'</span>'; 
+        
+        }
+      }
+    }
+    
+  }
+  
+  switch($path){
+    
+    case 'search':
+       lk_set_icon('search');
+       drupal_set_title("Suche");
+      break;
+    
+    
+    case 'dashboard':
+      lk_set_icon('th-large');
+      drupal_set_title("Startseite");
+      break;
+      
+    // Medien verwalten
+    case 'node/x/media':
+      lk_set("Medien verwalten", $node -> title, 'ok');
+     // lk_set_faq(7);
+      
+      
+      lk_set_subtitle($node -> title);
+      break;  
+  
+    case 'node/x/log':
+      lk_set("Aktionen",$node -> title, 'list');
+      //lk_set_faq(7);
+    
+      break;
+      
+      
+    case 'node/x/contact':
+      lk_set("Kontakt",$node -> title, 'user');
+      //lk_set_faq(7);
+    
+      break;  
+    
+    case 'node/x/stats':
+      lk_set("Statistiken",$node -> title, 'list');
+      //lk_set_faq(7);
+    
+      break; 
+    
+    case 'node/x/addmedia':
+      //lk_set_faq(7);
+      lk_set_icon('cloud-upload');
+      
+      if($node)  lk_set_subtitle($node -> title);
+      drupal_set_title("Upload hinzufügen");
+    
+      break;
+  
+    case 'node/add/kampagne':
+     //lk_set_faq(7);
+      drupal_set_title("Kampagne erstellen");
+      //lk_set_subtitle('Aussagekräftiger Untertitel');
+      lk_set_icon('plus');
+      break;
+    
+    
+    case 'node/x/delete':
+      //lk_set_faq(7);
+      //drupal_set_title("Kampagne erstellen");
+      lk_set_subtitle('Löschen bestätigen');
+      lk_set_icon('trash');
+      break;
+       
+    case 'node/kampagne/edit':
+       //lk_set_faq(7);
+      drupal_set_title('Kampagne editieren');
+      lk_set_subtitle($node -> title);
+      lk_set_icon('edit');
+      break;
+   
+    case 'node/x/plz/x/edit':
+      //lk_set_faq(7);
+      drupal_set_title('PLZ-Sperre editieren');
+      lk_set_subtitle($node -> title);
+      lk_set_icon('edit');
+      break;
+    
+   
+    
+    case 'node/x/plz':
+        //lk_set_faq(7);
+        drupal_set_title("Postleitzahl-Sperre einrichten");
+        lk_set_subtitle($node -> title);
+        lk_set_icon('edit');
+      break;
+    
+    case 'node/x/plz/x/delete':
+      lk_set_icon('trash');
+      lk_set_subtitle($node -> title);
+      drupal_set_title("PLZ-Regel löschen");
+      //lk_set_faq(7);
+    
+      break;  
+      
+    case 'node/x/media/x/edit':
+      lk_set_icon('edit');
+      lk_set_subtitle($node -> title);
+      drupal_set_title("Upload bearbeiten");
+      //lk_set_faq(7);
+      break;  
+  
+    case 'node/x/media/x/delete':
+      lk_set_icon('trash');
+      lk_set_subtitle($node -> title);
+      drupal_set_title("Upload löschen");
+      //lk_set_faq(7);
+      break;
+  
+    case 'node/x/presentation':
+      lk_set_icon('align-justify');
+      lk_set_subtitle($node -> title);
+      drupal_set_title("Präsentation einrichten");
+      //lk_set_faq(7);
+      break;
+      
+    case 'node/x/admin':
+      lk_set_icon('send');
+      lk_set_subtitle($node -> title);
+      drupal_set_title("Kampagne verwalten");
+      //lk_set_faq(7);
+      break;  
+    
+    
+    /********************************** User-Seiten  **********************************/
+      
+    // Userseiten
+    case 'user/pass':
+      lk_set_icon('flag');
+      //lk_set_subtitle('Passwort oder Kontakt-Email-Adresse ändern');
+      drupal_set_title("Passwort zurücksetzen");
+    
+      break;
+  
+  
+    case 'user/pass/reset':
+      lk_set_icon('flag');
+      //lk_set_subtitle('Einmalige Anmeldung');
+      drupal_set_title("Passwort zurücksetzen");
+    
+      break;
+    
+    
+  
+    case 'user/x/edit':
+      
+      lk_set_icon('wrench');
+      
+      
+      if(arg(3)){
+      
+      }
+      else {
+        //lk_set_subtitle('Passwort oder Kontakt-Email-Adresse ändern');
+        drupal_set_title("Passwort ändern");
+      }
+      
+      break;
+  
+    case 'user/x/edit/main':
+      lk_set_icon('edit');
+      //lk_set_subtitle('Persönliche Daten und Kontaktdaten');
+      drupal_set_title("Profildaten bearbeiten");
+      //lk_set_faq(7);
+      break;
+  
+    case 'user/x/kampagnen':
+      lk_set_icon('lock');
+      //lk_set_subtitle('Behalten Sie hier Ihre Kampagnen im Überblick');
+      drupal_set_title("Ihre Kampagnen");
+      //lk_set_faq(7);
+   
+      break;
+  
+    case 'node/x/preview':
+      lk_set_icon('lock');
+      lk_set_subtitle($node -> title);
+      drupal_set_title("Ihre Kampagne");
+      //lk_set_faq(7);
+      break;
+    
+     case 'node/x/status':
+       lk_set_subtitle($node -> title);
+       drupal_set_title("Ihre Kampagne");
+       //lk_set_faq(7);
+       lk_set_icon('lock');
+      break;
+    
+    
+    case 'messages':
+      lk_set_subtitle("Lokalkönig Messaging System");
+       //lk_set_faq(7);
+       lk_set_icon('envelope');
+      break;
+    
+    
+    
+    case 'merkliste':
+        lk_set_icon('tag');
+          drupal_set_title('Merklisten');
+    break;
+    
+    
+    case 'user/x/unteraccounts':
+      lk_set_icon('user');
+      drupal_set_title('Mitarbeiter-Accounts');
+      break;
+    
+      
+    default:
+      //lk_set_faq(7);
+      drupal_set_title('No Title');
+      lk_set_subtitle('für ' . $path);
+      lk_set_icon('edit');
+      
+    break;  
+    
+      
+      
+    
+  }
+  
+  if($special){
+    lk_set_subtitle(lk_get_subtitle() . " <small>". $special ."</small>");
+  } 
+}
+
+
+function _formlk(&$form){
+  $form["#title"] = drupal_get_title(); 
+  $form["#attributes"]["class"][] = 'panel panel-default panel-info';    
+}
+
+
+function _formlkdelete(&$form, $pathcancel){
+    $form["actions"]["cancel"]["#href"] = $pathcancel;
+    $form["actions"]["cancel"]["#options"]["path"] = $pathcancel;
+    $form['submit_redirect']["#value"] = $pathcancel;
+    
+    $form["description"]['#markup'] = '<div class="alert alert-warning">' . $form["description"]['#markup'] .'</div>';
+    
+    _formlk($form);   
+}
+
---
+++ main/depricated.inc.php
@@ -0,0 +1,216 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/** 
+ *   DEPRICATED
+ *   Erstellt eine Regel für die PLZ des Users   
+ *
+ **/
+function na_create_node_rule($nid, $uid, $until_date){
+  
+  $account = \LK\get_user($uid);
+  $ausgaben = $account -> getCurrentAusgaben();
+  
+  $manager = new \LK\Kampagne\SperrenManager();
+  $result = $manager ->createSperre($nid, $uid, $ausgaben, $until_date);  
+  
+  if($result){
+      return $result ->getId();
+  }  
+  
+  return false;    
+}
+
+
+/**
+ * 
+ * @deprecated since version number
+ * @param int $entity_id
+ * @return boolean
+ */
+function lokalkoenig_nodeaccess_delete_rule($entity_id){
+    
+    $manager = new \LK\Kampagne\SperrenManager();
+
+    return $manager ->removeSperre($entity_id);
+}
+
+
+
+
+
+
+ // checken ob eine Lizenz vorliegt
+function vku_user_has_lizenz_node($nid, $account){
+    $dbq = db_query("SELECT l.* FROM lk_vku vku, lk_vku_lizenzen l 
+          WHERE 
+            l.nid='". $nid ."' AND 
+            l.vku_id=vku.vku_id AND 
+            vku.vku_status='purchased' AND 
+            vku.uid='". $account -> uid  ."'");
+            
+  return $dbq -> fetchObject();
+}
+
+
+
+ // checken ob eine Lizenz vorliegt
+function node_has_lizenz_allready($nid){
+  $dbq = db_query("SELECT count(*) as count FROM lk_vku_lizenzen WHERE nid='". $nid ."'");
+  $result = $dbq -> fetchObject();
+  return $result -> count;
+
+}
+
+
+/** Ausgabe changed PLZ */
+function node_access_ausgabe_changed_plz($aid){
+    $ausgabe = \LK\get_ausgabe($aid);
+    $manager = new \LK\Kampagne\SperrenManager();
+    $manager ->updateAusgabe($ausgabe);
+}
+
+
+
+function _lokalkoenig_node_access_info_count($node){
+    \LK\Kampagne\AccessInfo::hasAccess($node);
+}
+
+
+function vku_get_use_count($nid, $account){
+    return \LK\Kampagne\AccessInfo::getAccessCount($nid, $account);    
+}
+
+function vku_get_use_details($nid, $account, $exclude = false){  
+    return \LK\Kampagne\AccessInfo::getUserDetails($nid, $account);   
+}
+
+
+function lokalkoenig_node_access_has_access($nid){
+   return \LK\Kampagne\AccessInfo::loadAccess($nid); 
+}
+
+
+function vku_get_use_count_days($account){
+
+return 10;   
+}
+
+
+
+function na_check_user_has_access($uid, $nid){
+global $user;
+  
+ $account = \LK\get_user($uid);
+ // If no Account
+ if(!$account) {
+    return false;
+ }
+ 
+ // No Node
+ $node = node_load($nid);
+ if(!$node){
+     return false;
+ }
+  
+ // Status of the Node is Offline
+ if($node -> lkstatus != 'published' OR $node -> status != 1) {
+      return array('access' => false, "reason" => "Kampagene nicht mehr Online.");
+ }
+ 
+ // No Ausgaben
+ $ausgaben = $account -> getCurrentAusgaben();
+ if(!$ausgaben){
+   return array('access' => true);  
+ }
+ 
+ $dbq = db_query("select until as date_until from na_node_access_ausgaben_time WHERE nid='". $nid ."' AND aid IN (". implode(",", $ausgaben) .") ORDER BY until DESC LIMIT 1"); 
+ $result = $dbq -> fetchObject();
+ 
+ if(!$result) return array('access' => true);
+ else {
+     return array('access' => false, 
+                  'time' => $result -> date_until,
+                  "reason" => "Die Kampagne ist ab dem ". date("d.m.Y", $result -> date_until) ." wieder verfügbar.");
+  }
+}
+
+
+/** Update Database on Entity Update */
+function lokalkoenig_nodeaccess_entity_update($entity, $type){
+    
+   // Ausgabe - PLZ Changed 
+   if($type == 'ausgabe'){
+     if($entity -> type == 'ausgabe'){
+         node_access_ausgabe_changed_plz($entity -> id);
+     }
+   }
+}
+
+/** INSERT Database on Entity Update  - AUsgabe*/
+function lokalkoenig_nodeaccess_entity_insert($entity, $type){
+  if($type == 'ausgabe'){
+    if($entity -> type == 'ausgabe'){
+      node_access_ausgabe_changed_plz($entity -> id);
+    }
+  }
+}
+
+
+
+
+
+
+function get_ausgaben_access_nid($nid,  $account){
+  
+  $ma = \LK\get_user($account);
+  if(!$ma){
+      return array();
+  }
+  
+  $verlag = $ma -> getVerlag();
+  if(!$verlag){
+      return array();
+  }
+  
+  $inverlag = array();
+  $dbq = db_query("SELECT ausgaben_id FROM na_node_access_ausgaben WHERE verlag_uid='". $verlag ."' AND nid='". $nid ."'");
+  foreach($dbq as $all){
+      $ausgabe = \LK\get_ausgabe($all -> ausgaben_id);
+      if($ausgabe){
+          $inverlag[] = $ausgabe -> getShortTitle(); 
+      }
+  }
+  
+  $outverlag = array();
+  $dbq = db_query("SELECT * FROM na_node_access_ausgaben WHERE verlag_uid != '". $verlag ."' AND nid='". $nid ."'");
+  foreach($dbq as $all){
+       $outverlag[] = $all -> plz_gebiet_aggregated;
+  }  
+   
+return array('count' => (count($inverlag) + count($outverlag)), 'in' => $inverlag, "out" => $outverlag);
+}
+
+
+function lkgetusername(){
+global $user;
+
+  if($user -> uid == 0) return 'Anonymus';
+ 
+  $profile = profile2_load_by_user($user);
+  
+  if(isset($profile['main']->field_profile_vorname['und'][0]['safe_value'])){
+     return $profile['main']->field_profile_vorname['und'][0]['safe_value'] . " " . $profile['main']->field_profile_name['und'][0]['safe_value'];   
+  }
+  
+  
+return 'No Name';
+}
+
+
